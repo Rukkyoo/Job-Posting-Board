2bb4394e436e7a7bd47c7456ce6c5da1d5c58680
Michael Omonedo <rukkyomonedo@gmail.com>
2025-02-21 11:51:46 +0100

    db connection

--- jobs-api/backend/app.js
+++ jobs-api/backend/app.js
@@ -2,6 +2,7 @@ import express from "express";
 const app = express();
 import authRouter from "./routes/auth.js";
 import jobsRouter from "./routes/jobs.js";
+import connectDB from "./db/connect.js";
 import dotenv from "dotenv";
 app.use(express.static("./src"));
 app.use(express.json());
@@ -16,6 +17,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI)
+    console.log("connected to the db")
     app.listen(port, () =>
       console.log(`Yes, the Server is listening on port ${port}...`)
     );
@@ -25,3 +28,5 @@ const start = async () => {
 };
 
 start();
+
+
