4a102a798e71ab55ecce3417da82ac62a6b1c0dc
Michael Omonedo <rukkyomonedo@gmail.com>
2025-02-21 18:52:59 +0100

    working on auth controller

--- jobs-api/backend/controllers/auth.js
+++ jobs-api/backend/controllers/auth.js
@@ -1,9 +1,20 @@
- export const register = async (req, res) => {
-  res.send("Register user");
+import User from "../models/User.js";
+import { StatusCodes } from "http-status-codes";
+import bcrypt from "bcryptjs";
+
+export const register = async (req, res) => {
+  const { name, email, password } = req.body
+  const salt = await bcrypt.genSalt(10)
+  const hashedPassword = await bcrypt.hash(password, salt)
+  const tempUser = { name, email, password: hashedPassword }
+  if (!name || !email || !password) {
+    return res.status(StatusCodes.BAD_REQUEST).json({ msg: "Please enter name, email and password" });
+  }
+  const user = await User.create({ ...tempUser });
+  res.status(StatusCodes.CREATED).json({ user });
 };
 
- export const login = async (req, res) => {
+export const login = async (req, res) => {
   res.send("Login user");
 };
-
 
---
+++ jobs-api/backend/models/User.js
@@ -0,0 +1,26 @@
+import mongoose from "mongoose";
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Please provide a name"],
+    minlength: 3,
+    maxlength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Please provide email"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    minlength: 6,
+  },
+});
+
+export default mongoose.models.User || mongoose.model("User", UserSchema);
