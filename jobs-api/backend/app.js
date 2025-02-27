e041be5fa81f73935097df4894c0ebd6cfc73a98
Michael Omonedo <rukkyomonedo@gmail.com>
2025-02-27 13:06:48 +0100

    minor frontend changes

--- jobs-api/backend/app.js
+++ jobs-api/backend/app.js
@@ -5,28 +5,31 @@ import jobsRouter from "./routes/jobs.js";
 import connectDB from "./db/connect.js";
 import authenticateUser from "./middleware/authentication.js";
 import dotenv from "dotenv";
-// extra security packages
-import helmet from "helmet"
-import xss from "xss-clean"
-import cors from "cors"
-import rateLimiter from "express-rate-limit"
-app.set("trust proxy", 1)
-app.use(rateLimiter({
-  windowMs: 15 * 60 * 1000, // 15 minutes
-	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
-	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-	// store: ... , // Redis, Memcached, etc. See below.
 
-}))
-app.use(express.static("./src"));
-app.use(express.json());
+// extra security packages
+import helmet from "helmet";
+import xss from "xss-clean";
+import cors from "cors";
+import rateLimiter from "express-rate-limit";
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+    standardHeaders: "draft-8", // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    // store: ... , // Redis, Memcached, etc. See below.
+  })
+);
+
+app.use(express.static("./src")); // serve static files
+app.use(express.json()); // parse json bodies
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
 
-app.use(helmet())
-app.use(cors())
-app.use(xss())
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 dotenv.config();
 app.get("/", (req, res) => {
@@ -37,8 +40,8 @@ const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
-    await connectDB(process.env.MONGO_URI)
-    console.log("connected to the db")
+    await connectDB(process.env.MONGO_URI);
+    console.log("connected to the db");
     app.listen(port, () =>
       console.log(`Yes, the Server is listening on port ${port}...`)
     );
@@ -48,7 +51,3 @@ const start = async () => {
 };
 
 start();
-
-
-
-
