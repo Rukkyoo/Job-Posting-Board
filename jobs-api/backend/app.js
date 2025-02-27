43d5a4aef7d399f89afdec3fb1d5d3e4cbc803c4
Michael Omonedo <rukkyomonedo@gmail.com>
2025-02-27 10:11:20 +0100

    implemented rate limiter

--- jobs-api/backend/app.js
+++ jobs-api/backend/app.js
@@ -5,10 +5,29 @@ import jobsRouter from "./routes/jobs.js";
 import connectDB from "./db/connect.js";
 import authenticateUser from "./middleware/authentication.js";
 import dotenv from "dotenv";
+// extra security packages
+import helmet from "helmet"
+import xss from "xss-clean"
+import cors from "cors"
+import rateLimiter from "express-rate-limit"
+app.set("trust proxy", 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+	// store: ... , // Redis, Memcached, etc. See below.
+
+}))
 app.use(express.static("./src"));
 app.use(express.json());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", authenticateUser, jobsRouter);
+
+app.use(helmet())
+app.use(cors())
+app.use(xss())
+
 dotenv.config();
 app.get("/", (req, res) => {
   res.send("Jobs api!");
